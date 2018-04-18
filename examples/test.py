2c6a1bead7e98ee8649d65aaa97696779b28b624
wwwins <jacky.huang@isobar.com>
2018-04-18 18:27:46 +0800

    Added python-dotenv

--- examples/test.py
+++ examples/test.py
@@ -4,11 +4,17 @@
 # Use text editor to edit the script and type in valid Instagram username/password
 
 from InstagramAPI import InstagramAPI
+from dotenv import load_dotenv
+import os
+import sys
 
-api = InstagramAPI("login", "password")
+load_dotenv(verbose=True, dotenv_path='./.env')
+
+api = InstagramAPI(os.getenv('USERNAME'), os.getenv('PASSWORD'))
 if (api.login()):
     api.getSelfUserFeed()  # get self user feed
     print(api.LastJson)  # print last response JSON
     print("Login succes!")
 else:
     print("Can't login!")
+ 
