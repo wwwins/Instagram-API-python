81d9a3e78928de7e60954b580567257798c572d2
wwwins <jacky.huang@isobar.com>
2018-05-28 09:59:19 +0800

    Created a session class

--- examples/session.py
+++ examples/session.py
@@ -12,51 +12,51 @@ import os
 import sys
 import pickle
 
-load_dotenv(verbose=True, dotenv_path='./.env')
-api = InstagramAPI(os.getenv('USERNAME'), os.getenv('PASSWORD'))
+class Session:
+    def __init__(self):
+        load_dotenv(verbose=True, dotenv_path='./.env')
+        self.api = InstagramAPI(os.getenv('USERNAME'), os.getenv('PASSWORD'))
+        self.loadSession()
+        if (self.api.isLoggedIn is not True):
+            if (self.api.login()):
+                print("Login success!!")
+                self.saveSession(self.api)
+            else:
+                print("Can't login!")
+        else:
+            if(self.api.isLoggedIn):
+                print("reLogin success!!!")
 
-def genSessionObj(obj):
-    return {
-        'token': obj.token,
-        'username_id': obj.username_id,
-        'rank_token': obj.rank_token,
-        'uuid': obj.uuid,
-        'session': obj.s.cookies.get_dict(),
-        'device_id': obj.device_id
-    }
 
-def saveSession(apiObj):
-    with open('session.pkl','wb') as f:
-        print('Save session.pkl')
-        pickle.dump(genSessionObj(apiObj), f)
+    def genSessionObj(self, obj):
+        return {
+            'token': obj.token,
+            'username_id': obj.username_id,
+            'rank_token': obj.rank_token,
+            'uuid': obj.uuid,
+            'session': obj.s.cookies.get_dict(),
+            'device_id': obj.device_id
+        }
 
-def loadSession():
-    if Path('session.pkl').is_file():
-        with open('session.pkl', 'rb') as f:
-            print('Load session.pkl')
-            load_session = pickle.load(f)
-            api.token = load_session['token']
-            api.username_id = load_session['username_id']
-            api.rank_token = load_session['rank_token']
-            api.uuid = load_session['uuid']
-            api.s.cookies = cookiejar_from_dict(load_session['session'])
-            api.device_id = load_session['device_id']
-            api.isLoggedIn = True
-    else:
-        api.isLoggedIn = False
+    def saveSession(self, apiObj):
+        with open('session.pkl','wb') as f:
+            print('Save session.pkl')
+            pickle.dump(self.genSessionObj(apiObj), f)
 
-# relogin
-loadSession()
-if (api.isLoggedIn is not True):
-    if (api.login()):
-        api.getSelfUserFeed()  # get self user feed
-        print(api.LastJson)  # print last response JSON
-        print("Login success!!")
-        saveSession(api)
-    else:
-        print("Can't login!")
-else:
-    if(api.isLoggedIn):
-        print("reLogin success!!!")
-        api.getSelfUserFeed()
-        print(api.LastJson)
+    def loadSession(self):
+        if Path('session.pkl').is_file():
+            with open('session.pkl', 'rb') as f:
+                print('Load session.pkl')
+                load_session = pickle.load(f)
+                self.api.token = load_session['token']
+                self.api.username_id = load_session['username_id']
+                self.api.rank_token = load_session['rank_token']
+                self.api.uuid = load_session['uuid']
+                self.api.s.cookies = cookiejar_from_dict(load_session['session'])
+                self.api.device_id = load_session['device_id']
+                self.api.isLoggedIn = True
+        else:
+            self.api.isLoggedIn = False
+
+if __name__ == "__main__":
+    sess = Session()
